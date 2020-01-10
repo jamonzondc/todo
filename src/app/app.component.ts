a93e0ad42420f1e34811eb256228cfb88ab5b2a4
jamonzondc <54319358+jamonzondc@users.noreply.github.com>
2020-01-10 18:33:28 -0500

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Plyr from 'plyr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+
+  ngAfterViewInit(): void {
+    const player = new Plyr('#player', {});
+  }
   title = 'todo';
+
+
 }
